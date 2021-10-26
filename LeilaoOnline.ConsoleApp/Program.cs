c873c7a6bc1f2228b29eb7e822e4a19177595cf9
derciosinione <derciosinione@gmail.com>
2021-10-26 18:32:03 +0100

    Added ConsoleApp est

---
+++ LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using LeilaoOnline.Core;
+
+namespace LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arange
+            var leilao = new Leilao("Dr Cash");
+            var jorge = new Interessada("Jorge", leilao);
+            var maria = new Interessada("Maria", leilao);
+            
+            leilao.RecebeLance(jorge, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(jorge, 1000);
+            leilao.RecebeLance(maria, 950);
+
+            // Act
+            leilao.TerminaPregao();
+            
+            // Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            if (valorEsperado == valorObtido)
+            {
+                Console.WriteLine("TEST OK");
+            }
+            else
+            {
+                Console.WriteLine("TEST FALHOU");
+            }
+        }
+    }
+}
--- LeilaoOnline.Core/Leilao.cs
+++ LeilaoOnline.Core/Leilao.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeilaoOnline.Core
 {
@@ -7,6 +8,7 @@ namespace LeilaoOnline.Core
         private IList<Lance> _lances;
         public IEnumerable<Lance> Lances => _lances;
         public string Peca { get; }
+        public Lance Ganhador { get; private set; }
 
         public Leilao(string peca)
         {
@@ -26,7 +28,7 @@ namespace LeilaoOnline.Core
 
         public void TerminaPregao()
         {
-
+            Ganhador = Lances.Last();
         }
     }
 }
