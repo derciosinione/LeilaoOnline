78c225f85a0cab7968f22f03b6d729b55a13a08b
derciosinione <derciosinione@gmail.com>
2021-10-26 17:28:12 +0100

    Added Class, Interessasa, Lance, Leilao

---
+++ LeilaoOnline.Core/Lance.cs
@@ -0,0 +1,14 @@
+namespace LeilaoOnline.Core
+{
+    public class Lance
+    {
+        public Interessada Cliente { get; }
+        public double Valor { get; }
+
+        public Lance(Interessada cliente, double valor)
+        {
+            Cliente = cliente;
+            Valor = valor;
+        }
+    }
+}
