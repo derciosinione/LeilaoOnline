9206564aeff9a51f67cfc8806a9384f959ad7590
derciosinione <derciosinione@gmail.com>
2021-10-26 20:00:52 +0100

    Added Verification Method for Leilao

--- LeilaoOnline.UnitTest/LeilaoTest.cs
+++ LeilaoOnline.UnitTest/LeilaoTest.cs
@@ -6,8 +6,21 @@ namespace LeilaoOnline.UnitTest
     public class LeilaoTest
     {
         [Fact]
-        public void Test1()
+        public void LeilaoComVariosLances()
         {
+        }
+        
+        [Fact]
+        public void LeilaoComUmLance()
+        {
+        }
+        
+        private static void VerifyLeilao(double valorEsperado, double valorObtido)
+        {
+            if (valorEsperado == valorObtido)
+                Console.WriteLine("TEST OK");
+            else
+                Console.WriteLine($"TEST FALHOU! Valor Esperado:{valorEsperado}, Valor Obtido: {valorObtido}");
         }
     }
 }
